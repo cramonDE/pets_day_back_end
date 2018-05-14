3e0347a33f2253d5f4e4d34307a874f9e8f7dbbd
xiezifeng <xiezifeng@corp.netease.com>
2018-05-15 00:37:11 +0800

    first commit

---
+++ models/user.model.go
@@ -0,0 +1,44 @@
+package models
+
+import "github.com/astaxie/beego/orm"
+
+type User struct {
+	UserId       int    `json:"user_id"`
+	Password     string `json:"password"`
+	Username     string `json:"username"`
+	UserNickname string `json:"user_nickname"`
+}
+
+func GetUsers() (*[]User, error) {
+	o := orm.NewOrm()
+	var users []User
+	_, err := o.Raw("SELECT * FROM user").QueryRows(&users)
+	return &users, err
+}
+
+func InsertUser(user *User) (int64, error) {
+	o := orm.NewOrm()
+	res, err := o.Raw("INSERT INTO user (username, password, user_nickname) VALUES (?, ?, ?);", user.Username, user.Password, user.UserNickname).Exec()
+	insertId, _ := res.LastInsertId()
+	return insertId, err
+}
+
+func UpdateUser(user *User, id int) error {
+	o := orm.NewOrm()
+	_, err := o.Raw("UPDATE user SET password = ?, user_nickname = ? WHERE user_id = ?;", user.Password, user.UserNickname, id).Exec()
+	return err
+}
+
+func CheckUser(user *User) (int, error) {
+	o := orm.NewOrm()
+	var users []User
+	_, err := o.Raw("SELECT * FROM user WHERE username = ? && password = ?;", user.Username, user.Password).QueryRows(&users)
+	return len(users), err
+}
+
+func GetOneUser(username string) (User, error) {
+	o := orm.NewOrm()
+	var user User
+	err := o.Raw("SELECT * FROM user WHERE username = ?;", username).QueryRow(&user)
+	return user, err
+}
